b6c3d1e00339d338fd71151c2f04bec1519b2216
Hirotake Kobayashi <h.kobayashi@raksul.com>
2021-03-29 02:55:31 +0900

    Add terraform-plugin-docs

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+//go:generate terraform fmt -recursive ./examples/
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	var debugMode bool
 
